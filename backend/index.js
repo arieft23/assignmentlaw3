fb97cba8300b4539398c675dc54838a80ff55e76
arieft23 <arieftritomo@gmail.com>
2019-03-30 10:50:34 +0700

    update baru apa tau

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ const app = express()
 app.use(fileupload())
 let channel = null
 
-const handleUpload = (filename, totalSize, callback) =>{
-    setTimeout(()=>{
+const handleUpload = (filename, totalSize, callback) => {
+    setTimeout(() => {
         var archive = archiver('zip', {
             zlib: { level: 9 } // Sets the compression level.
         });
@@ -22,22 +22,22 @@ const handleUpload = (filename, totalSize, callback) =>{
         //     callback(archive.pointer())
         // })
         let percent = 10
-        archive.on("data", (chunk)=>{
-            if(archive.pointer()/totalSize*100 >= percent){
-                callback(percent)
+        archive.on("data", (chunk) => {
+            if (archive.pointer() / totalSize * 100 >= percent) {
+                callback("Progress : %s", percent)
                 percent += 10
             }
         })
 
-        archive.on("end", ()=>{
+        archive.on("end", () => {
             fs.unlink(location + filename, err => {
-                if(err) console.log(err)
+                if (err) console.log(err)
                 else return callback("Data has been compressed")
             })
         })
 
         archive.pipe(output)
-        archive.append(input, {name : filename})
+        archive.append(input, { name: filename })
         archive.finalize()
     }, 0)
 }
@@ -55,32 +55,54 @@ const handleUpload = (filename, totalSize, callback) =>{
 //     conn.createChannel(function(err, ch) {
 //         var ex = 'exchange_ping';
 //         var msg = 'Hello World! ini Arief';
-    
+
 //         ch.assertExchange(ex, 'fanout', {durable: false});
 //         while(true){
 //             ch.publish(ex, '', new Buffer(msg));
 //             console.log(" [x] Sent %s", msg);
 //         }
-        
+
 //       });
 //     //   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 // })
 
-app.post("/upload", (req,res)=>{
+app.post("/upload", (req, res) => {
     const file = req.files.file
     const filename = file.name
     const totalSize = file.size
-    file.mv(location + file.name, err =>{
-        if(err){
+    file.mv(location + file.name, err => {
+        if (err) {
             return res.status(500).send("ada error")
         }
-        handleUpload(filename, totalSize, (response)=>{
-            console.log(response)
+        handleUpload(filename, totalSize, (response) => {
+            handlePublish(response, "test")
         })
-        res.json({status: "ok"})
+        res.json({ status: "ok" })
     })
 })
 
-app.listen(3001, ()=>{
-    console.log("Server runnning on port 3000")
+const handlePublish = (msg, routeKey) =>{
+    const ex = "1506689061"
+    channel.assertExchange(ex, "direct", {durable: true})
+    channel.publish(ex, routeKey, Buffer.from(msg))
+}
+
+amqplib.connect({
+    protocol: "amqp",
+    hostname: "localhost",
+    port: "5672",
+    username: "1506689061",
+    password: "arief",
+    vhost: "1506689061"
+}, (err, conn) => {
+    if(err){
+        return "ws rusak"
+    }
+    conn.createChannel((err, ch)=>{
+        if(err) return "ws rusak"
+        channel = ch
+        app.listen(3001, () => {
+            console.log("Server runnning on port 3000")
+        })
+    })
 })
--- frontend/index.js
+++ frontend/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const fileupload = require('express-fileupload')
 const request = require('request')
+const cors = require('cors')
 
 app.use(fileupload())
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(express.static('public'))
 
